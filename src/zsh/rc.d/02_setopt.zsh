9b86a188cf2d23e2c81bf39139a049743b824ab7
Chad Durkin <DurkinDevelopment@gmail.com>
2022-01-01 06:48:58 -0800

    feat: Added the setopt zsh config rc.d - z0rc

---
+++ src/zsh/rc.d/02_setopt.zsh
@@ -0,0 +1,29 @@
+setopt HIST_IGNORE_ALL_DUPS # remove all earlier duplicate lines
+setopt APPEND_HISTORY # history appends to existing file
+setopt SHARE_HISTORY # import new commands from the history file also in other zsh-session
+setopt EXTENDED_HISTORY # save each commands beginning timestamp and the duration to the history file
+setopt HIST_REDUCE_BLANKS # trim multiple insgnificant blanks in history
+setopt HIST_IGNORE_SPACE # don’t store lines starting with space
+setopt EXTENDED_GLOB # treat special characters as part of patterns
+setopt CORRECT_ALL # try to correct the spelling of all arguments in a line
+setopt NO_FLOW_CONTROL # disable stupid annoying keys
+setopt MULTIOS # allows multiple input and output redirections
+setopt AUTO_CD # if the command is directory and cannot be executed, perfort cd to this directory
+setopt CLOBBER # allow > redirection to truncate existing files
+setopt BRACE_CCL # allow brace character class list expansion
+setopt NO_BEEP # do not beep on errors
+setopt NO_NOMATCH # try to avoid the 'zsh no matches found...'
+setopt INTERACTIVE_COMMENTS # allow use of comments in interactive code
+setopt AUTO_PARAM_SLASH # complete folders with / at end
+setopt LIST_TYPES # mark type of completion suggestions
+setopt HASH_LIST_ALL # whenever a command completion is attempted, make sure the entire command path is hashed first
+setopt COMPLETE_IN_WORD # allow completion from within a word/phrase
+setopt ALWAYS_TO_END # move cursor to the end of a completed word
+setopt LONG_LIST_JOBS # list jobs in the long format by default
+setopt AUTO_RESUME # attempt to resume existing job before creating a new process
+setopt NOTIFY # report status of background jobs immediately
+unsetopt RM_STAR_SILENT # notify when rm is running with *
+setopt RM_STAR_WAIT # wait for 10 seconds confirmation when running rm with *
+
+# a bit fancy than default
+PROMPT_EOL_MARK='%K{red} %k':
