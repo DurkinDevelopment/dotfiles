964fcff86d3e6f5ae31f467b5581eb976891e020
Chad Durkin <DurkinDevelopment@gmail.com>
2022-01-16 06:38:40 -0800

    feat: Extracted the export of the env variables to the env.d export file

--- post-install.sh
+++ post-install.sh
@@ -3,20 +3,13 @@
 # Any post-install shell script functions go HERE
 # Use the post-install.sh to source any environment variables
 set_environment_variables() {
-	set_env_var_PATH
-	set_env_var_GOPATH
 	set_env_var_DOTDIR
 	set_env_var_ZDOTDIR
-	set_env_var_NVIM_LOG_FILE
 }
 
 set_env_var_PATH() {
 	#export PATH=/bin:/usr/bin:/usr/local/bin:${PATH}
 	#export PATH=${HOME}/.local/bin:${HOME}/.node_modules/bin:${HOME}/.cargo/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/bin/core_perl:${HOME}/bin:${HOME}/google-cloud-sdk/bin
-}
-
-set_env_var_GOPATH() {
-	export GOPATH=${HOME}
 }
 
 set_env_var_DOTDIR() {
@@ -34,10 +27,6 @@ set_env_var_ZDOTDIR() {
     		ln -sf "${SCRIPT_DIR}/zsh/.zshenv" "${ZDOTDIR:-${HOME}}/.zshenv"
    		echo "  ...failed to match this script dir, symlinking .zshenv"
 	fi
-}
-
-set_env_var_NVIM_LOG_FILE() {
-	export NVIM_LOG_FILE = ${HOME}/.cache/nvim/log
 }
 
 
